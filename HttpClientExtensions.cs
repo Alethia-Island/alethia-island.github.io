e05d89fc4f5004b9511c1ecd23a762d42d1c3603
TBG Renfold <tbg.renfold@gmail.com>
2021-07-24 19:11:09 +0100

    FIrst commit (make sure everything works out).

---
+++ HttpClientExtensions.cs
@@ -0,0 +1,20 @@
+﻿using CommonMark;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AlethiaIsland
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<string> GetMarkdownAsHtml(this HttpClient client, string uri) => await GetMarkdownAsHtml(client, new Uri(uri));
+        public static async Task<string> GetMarkdownAsHtml(this HttpClient client, Uri uri)
+        {
+            var resp = await client.GetAsync(uri);
+            if (resp.IsSuccessStatusCode is true)
+                return new(CommonMarkConverter.Convert(await resp.Content.ReadAsStringAsync()));
+
+            return string.Empty;
+        }
+    }
+}
