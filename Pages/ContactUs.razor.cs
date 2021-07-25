34ee879924304af0beb74e0ed652014ca30f9582
TBG Renfold <tbg.renfold@gmail.com>
2021-07-25 16:23:34 +0100

    Add Contact Us page.

---
+++ Pages/ContactUs.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AlethiaIsland.Pages
+{
+    public partial class ContactUs
+    {
+        [Inject]
+        private HttpClient Http { get; set; }
+
+        private MarkupString Output = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Output = new(await Http.GetMarkdownAsHtml($"{AppConfig.LinksRepo}/contact_us.md"));
+            await base.OnInitializedAsync();
+        }
+    }
+}
