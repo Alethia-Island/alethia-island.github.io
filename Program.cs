b39878bf105888335d33eaa818161ea845527193
TBG Renfold <tbg.renfold@gmail.com>
2021-10-09 23:25:37 +0100

    Update to DotNet 6 Rc 1.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace AlethiaIsland
@@ -8,7 +9,7 @@ namespace AlethiaIsland
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
+            builder.RootComponents.Add<HeadOutlet>("head::after");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
